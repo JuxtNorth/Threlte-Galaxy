accb0bc2af277d9a5eca0ea8453669bf11932ef7
North <deykiron365@gmail.com>
2023-10-26 17:05:12 +0530

    Release V0.0.1

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,81 +1,152 @@
 <script lang="ts">
-  import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+  import { 
+    T,
+    useThrelte,
+    useRender 
+  } from '@threlte/core'
+  import {
+    OrbitControls,
+    Instance,
+    InstancedMesh
+  } from '@threlte/extras';
+  import {
+    EffectComposer,
+    EffectPass,
+    RenderPass,
+    BloomEffect,
+    KernelSize
+  } from 'postprocessing';
+  
+  const { scene, renderer, camera, size } = useThrelte()
+  const composer = new EffectComposer(renderer)
+  const setupEffectComposer = (camera) => {
+    composer.removeAllPasses()
+    composer.addPass(new RenderPass(scene, camera))
+    composer.addPass(
+      new EffectPass(
+        camera,
+        new BloomEffect({
+          intensity: 2,
+          luminanceThreshold: 0.08,
+          height: 512,
+          width: 512,
+          luminanceSmoothing: 0.08,
+          mipmapBlur: true,
+          kernelSize: KernelSize.MEDIUM
+        })
+      )
+    )
+  }
+  // We need to set up the passes according to the camera in use
+  $: setupEffectComposer($camera)
+  $: composer.setSize($size.width, $size.height)
+  useRender((_, delta) => {
+    composer.render(delta)
+  })
+  
+  const count = 20000;
+  const instances = generate(count);
+  
+  function generate(count) {
+    const origin = vec3();
+    const noise = vec3();
+    const noiseFactor = 144;
+    const thetaFactor = 1.00048;
+    const points = new Array(count);
+    
+    let theta = 0.1, dist = 0.1;
+    
+    // Arm 1
+    for (let i = 0; i < count / 2; i++) {
+      randomise(noise, noiseFactor);
+      const x = origin[0] + noise[0] + Math.cos(theta) * dist;
+      const y = origin[1] + noise[1];
+      const z = origin[2] + noise[2] + Math.sin(theta) * dist;
+      theta *= thetaFactor;
+      dist += 0.028 + (dist * 0.0002);
+      points[i] = [
+        ...vec3(x, y, z),
+        ...getColor(dist)
+       ];
+    }
+    
+    dist = 0.1, theta = 0.1;
+    // Arm 2 
+    for (let i = count / 2; i < count; i++) {
+      randomise(noise, noiseFactor);
+      const x = origin[0] + noise[0] + -Math.cos(theta) * dist;
+      const y = origin[1] + noise[1];
+      const z = origin[2] + noise[2] + -Math.sin(theta) * dist;
+      
+      theta *= thetaFactor;
+      dist += 0.028 + (dist * 0.0002);
+      points[i] = [
+        ...vec3(x, y, z),
+        ...getColor(dist)
+       ];
+    }
+    
+    return points;
+    
+  }
+  
+  function vec3(x = 0, y = 0, z = 0) {
+    return [ x, y, z ];
+  }
+  
+  function scale(vector, scalar) {
+    vector[0] *= scalar;
+    vector[1] *= scalar;
+    vector[2] *= scalar;
+    return vector;
+  }
+  
+  function randomise(vector, scalar) {
+    vector[0] = Math.random();
+    vector[1] = Math.random();
+    vector[2] = Math.random();
+    const dist = (-1 + Math.random() * 2) * scalar;
+    return scale(vector, dist);
+  }
+  
+  function getColor(dist) {
+    const colorA = vec3(0.12, 0.13, 1.00);
+    const colorB = vec3(1.00, 0.12, 0.28);
+    const max = 900;
+    const normalised = dist / max;
+    return vec3(
+      lerp(colorA[0], colorB[0], normalised),
+      lerp(colorA[1], colorB[1], normalised),
+      lerp(colorA[2], colorB[2], normalised)
+    );
+  }
+  
+  function lerp(a, b, t) {
+    return a + (b - a) * t;
+  }
+  
 </script>
 
 <T.PerspectiveCamera
   makeDefault
-  position={[-10, 10, 10]}
-  fov={15}
+  position={300}
+  far={20000}
+  on:create={({ ref }) => {
+    ref.lookAt(0, 1, 0)
+  }}
 >
-  <OrbitControls
-    autoRotate
-    enableZoom={false}
-    enableDamping
-    autoRotateSpeed={0.5}
-    target.y={1.5}
-  />
+  <OrbitControls enableDamping />
 </T.PerspectiveCamera>
 
-<T.DirectionalLight
-  intensity={0.8}
-  position.x={5}
-  position.y={10}
-/>
-<T.AmbientLight intensity={0.2} />
-
-<Grid
-  position.y={-0.001}
-  cellColor="#ffffff"
-  sectionColor="#ffffff"
-  sectionThickness={0}
-  fadeDistance={25}
-  cellSize={2}
-/>
-
-<ContactShadows
-  scale={10}
-  blur={2}
-  far={2.5}
-  opacity={0.5}
-/>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position.y={1.2}
-    position.z={-0.75}
-  >
-    <T.BoxGeometry />
-    <T.MeshStandardMaterial color="#0059BA" />
-  </T.Mesh>
-</Float>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[1.2, 1.5, 0.75]}
-    rotation.x={5}
-    rotation.y={71}
-  >
-    <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-    <T.MeshStandardMaterial color="#F85122" />
-  </T.Mesh>
-</Float>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
-    <T.IcosahedronGeometry />
-    <T.MeshStandardMaterial color="#F8EBCE" />
-  </T.Mesh>
-</Float>
+<InstancedMesh limit={count} range={count}>
+  <T.BoxGeometry args={[ 2, 2, 2 ]} />
+  <T.MeshBasicMaterial />
+  
+  {#each instances as [ x, y, z, r, g, b ]}
+    <Instance 
+      position={[x, y, z]}
+      color={[r, g, b]}
+    />
+  {/each}
+  
+</InstancedMesh>
